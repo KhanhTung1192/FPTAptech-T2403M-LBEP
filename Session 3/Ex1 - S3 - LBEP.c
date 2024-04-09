675babffc0e0b91d99b52d64bc7e8726cd58babb
Khánh Tùng <tunglk1192@gmail.com>
2024-04-09 23:13:46 +0700

    Update VD

--- Session 3/Ex1 - S3 - LBEP.c
+++ Session 3/Ex1 - S3 - LBEP.c
@@ -1,3 +1,10 @@
-//
-// Created by admin on 4/9/2024.
-//
+#include <stdio.h>
+
+int main() {
+    int a, sum;
+    printf("Enter the number: ");
+    scanf("%d", &a);
+    sum = a * a;
+    printf("Binh phuong cua %d = %d", a, sum);
+    return 0;
+}
