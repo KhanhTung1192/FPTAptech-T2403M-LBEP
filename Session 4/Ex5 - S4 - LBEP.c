3860280905657492d19967e724aee61122aee204
Khánh Tùng <tunglk1192@gmail.com>
2024-04-09 23:50:02 +0700

    Hoàn thành bài tập

--- Session 4/Ex5 - S4 - LBEP.c
+++ Session 4/Ex5 - S4 - LBEP.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    float a = 12000, b, c = 150, d = 120, e = 450, f, g, z, sum;
+    float base = 12000, DA, HRA = 150, TA = 120, Others = 450, PF, IT, Tax, sum;
     printf("Employee's Salary: \n");
-    b = 0.12 * a;
-    f = 0.14 * a;
-    g = 0.15 * a;
-    z = f + g;
-    sum = a + b + c + d + e;
-    printf("Based salary = $%.2f\n", a);
-    printf("DA = %.2f\n", b);
-    printf("HRA = %.2f\n", c);
-    printf("TA = %.2f\n", d);
-    printf("Other = %.2f\n", e);
-    printf("PF = %.2f\n", f);
-    printf("IT = %.2f\n\n", g);
+    DA = 0.12 * base;
+    PF = 0.14 * base;
+    IT = 0.15 * base;
+    Tax = PF + IT;
+    sum = base + DA + HRA + TA + Others;
+    printf("Based salary = $%.2f\n", base);
+    printf("DA = %.2f\n", DA);
+    printf("HRA = %.2f\n", HRA);
+    printf("TA = %.2f\n", TA);
+    printf("Others = %.2f\n", Others);
+    printf("PF = %.2f\n", PF);
+    printf("IT = %.2f\n\n", IT);
     printf("Before Tax = %.2f\n", sum);
-    printf("Tax = %.2f + %.2f = $%.2f\n", f, g,-z);
-    printf("Salary Total = $%.2f", sum-z);
+    printf("Tax = %.2f + %.2f = $%.2f\n", PF, IT,-Tax);
+    printf("Salary Total = $%.2f", sum-Tax);
     return 0;
 }
