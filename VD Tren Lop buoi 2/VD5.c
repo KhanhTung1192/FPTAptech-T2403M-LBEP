864b3c187d2c93ceeac81d105a4f2a9cc958e4ce
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-10 12:02:59 +0700

    Hoan thanh bai 1 2 3 tai lop

---
+++ VD Tren Lop buoi 2/VD5.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int a=5,b=10,c=7;
+//     int tong, hieu, tich, thuong;
+//     //Toan tu toan hoc
+//     tong = a+b;
+//     hieu = a-b;
+//     tich = a*b;
+//     thuong = a/b;
+//     //Toan tu quan he
+//     int ketqua1,ketqua2,ketqua3,ketqua4;
+//     ketqua1=(a>b);
+//     ketqua2=(a<=b);
+//     ketqua3=(a==c);
+//     ketqua4=(a!=c);
+//     //Toan tu logic
+//     int ketqua5,ketqua6,ketqua7;
+//     ketqua5= (a<b && b>c);
+//     ketqua6= (a<b || b<c);
+//     ketqua7= !(a<b);
+//     //in ket qua
+//     printf("Tong: %d\n",tong);
+//     printf("tong: %d\n",tong);
+//     printf("tich: %d\n",tich);
+//     printf("thuong: %d\n",thuong);
+//     printf("ketqua1: %d\n",ketqua1);
+//     printf("ketqua2: %d\n",ketqua2);
+//     printf("ketqua3: %d\n",ketqua3);
+//     printf("ketqua4: %d\n",ketqua4);
+//     printf("ketqua5: %d\n",ketqua5);
+//     printf("ketqua6: %d\n",ketqua6);
+//     printf("ketqua7: %d\n",ketqua7);
+//
+//     return 0;
+// }
