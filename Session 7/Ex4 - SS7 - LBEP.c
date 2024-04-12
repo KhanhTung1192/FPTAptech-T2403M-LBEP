533547ff158688aee0958ac36a17fcad8f2e04f7
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-12 09:28:56 +0700

    Lam VD 4 bai 7

---
+++ Session 7/Ex4 - SS7 - LBEP.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// int main()
+// {
+//     //INPUT
+//     char type;
+//     int salary,allowances;
+//     printf("INPUT Your employee type(A,B,others):");
+//     scanf("%c",&type);
+//     printf("INPUT Your employee salary:");
+//     scanf("%d",&salary);
+//     //OUTPUT
+//     if (type=='A'||type=='a')
+//         printf("\nYour allowances is: $%d",salary+300);
+//     else if (type=='B'||type=='b')
+//         printf("\nYour allowances is: $%d",salary+250);
+//     else
+//         printf("\nYour allowances is: $%d",salary+100);
+//     return 0;
+// }
