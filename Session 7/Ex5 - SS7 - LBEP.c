5645d39be87cf156e1b4317cf7ea20078d9748be
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-12 09:34:47 +0700

    Lam VD 4 bai 7

---
+++ Session 7/Ex5 - SS7 - LBEP.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    //INPUT
+    char grade;
+    int score;
+    printf("INPUT score");
+    scanf("%d",&score);
+    //OUTPUT
+        if (score>75)
+        grade = 'A';
+    else if (score>65 && score<=75)
+        grade = 'B';
+    else if (score>45 && score<=60)
+        grade = 'C';
+    else if (score>35 && score<=45)
+        grade = 'D';
+    else
+        grade = 'E';
+    printf("Your grade is: %c",grade);
+    return 0;
+}
