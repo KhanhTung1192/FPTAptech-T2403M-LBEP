1c3d027e907c81940906e38dab7260e0cf295c5e
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-12 08:27:03 +0700

    Lam VD 1 bai 7

---
+++ Session 7/VD1 - SS7 - LBEP.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+//
+// int main()
+// {
+//     //Input
+//     int num;
+//     printf("Input N number: ");
+//     scanf("d%",&num);
+//     if(num%2==1)
+//         printf("\nYour number %d is \"Odd\" num:",num);
+//     else
+//         printf("\nYour number %d is \"Even\" num:",num);
+//     return 0;
+// }
--- VD Tren Lop buoi 2/VD6.c
+++ VD Tren Lop buoi 2/VD6.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
-
-int main()
-{
-    int a=5,b=10,c=7;
-    printf("a=", a);
-    printf("b=", b);
-    printf("c=", c);
-    //Tong hop cac loaij toan tu
-    printf("bieu thuc vi du la: a+b<c && a!=b\n");
-    printf("ket qua bieu thuc (%d+%d<%d && %d!=%d)\n", a,b,c,a,b);
-    int ketqua = a+b<c && a!=b;
-    printf("Ket qua:%d\n", ketqua);
-
-    printf("bieu thuc vi du la: a+b>c || a!=b\n");
-    printf("ket qua bieu thuc (%d+%d>%d || %d!=%d)\n", a,b,c,a,b);
-    int ketqua2 = a+b>c || a!=b;
-    printf("Ket qua:%d\n", ketqua2);
-
-    return 0;
-}
+// #include<stdio.h>
+//
+// int main()
+// {
+//     int a=5,b=10,c=7;
+//     printf("a=", a);
+//     printf("b=", b);
+//     printf("c=", c);
+//     //Tong hop cac loaij toan tu
+//     printf("bieu thuc vi du la: a+b<c && a!=b\n");
+//     printf("ket qua bieu thuc (%d+%d<%d && %d!=%d)\n", a,b,c,a,b);
+//     int ketqua = a+b<c && a!=b;
+//     printf("Ket qua:%d\n", ketqua);
+//
+//     printf("bieu thuc vi du la: a+b>c || a!=b\n");
+//     printf("ket qua bieu thuc (%d+%d>%d || %d!=%d)\n", a,b,c,a,b);
+//     int ketqua2 = a+b>c || a!=b;
+//     printf("Ket qua:%d\n", ketqua2);
+//
+//     return 0;
+// }
