d5d2e767134f2814206a88e89214d2ef6620c2c6
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-12 08:35:37 +0700

    Lam VD 2 bai 7

---
+++ Session 7/VD2 - SS7 - LBEP.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+//
+// int main()
+// {
+//     int a,b;
+//     char c='b';
+//     a=b=0;
+//     if (c == 'b')
+//     {
+//         a+=5;
+//         printf("The numbers are %d and %d",a,b);
+//     }
+// }
