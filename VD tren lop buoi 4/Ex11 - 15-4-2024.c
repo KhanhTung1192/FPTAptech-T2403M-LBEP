002cba4a971141e0eb43e2ad73f079c9dd45b9f2
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-15 11:51:58 +0700

    15-04-2024 bai viet

---
+++ VD tren lop buoi 4/Ex11 - 15-4-2024.c
@@ -0,0 +1,3 @@
+//
+// Created by Nakai on 15/04/2024.
+//
---
+++ VD tren lop buoi 4/Ex8 - 15-4-2024.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c,x,delta;
+    printf("INPUT a b c of \"ax2+bx+c=0\": ");
+    scanf("%f,%f,%f",&a,&b,&c);
+    if (delta)
+}
