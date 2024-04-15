1d467a9abe42c43b35ccb4f210a11d42adb1af52
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-15 09:49:19 +0700

    15-04-2024 bai viet

--- VD tren lop buoi 4/Ex3 - 15-4-2024.c
+++ VD tren lop buoi 4/Ex3 - 15-4-2024.c
@@ -1,3 +1,14 @@
-//
-// Created by Nakai on 15/04/2024.
-//
+#include <stdio.h>
+int main()
+{
+    float A,B,C;
+    printf("Input A,B,C: ");
+    scanf("%f,%f,%f",&A,&B,&C);
+    if (A>B && A>C)
+        printf("Max is A: %.2f",A);
+    else if (B>A && B>C)
+        printf("Max is B: %.2f",B);
+    else
+        printf("Max is C: %.2f",C);
+    return 0;
+}
