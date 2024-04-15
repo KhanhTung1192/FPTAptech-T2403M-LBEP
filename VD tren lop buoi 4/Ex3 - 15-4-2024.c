69e373111234721f80b6232faf89842c54f5e218
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-15 09:59:42 +0700

    15-04-2024 bai viet

--- VD tren lop buoi 4/Ex3 - 15-4-2024.c
+++ VD tren lop buoi 4/Ex3 - 15-4-2024.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-int main()
-{
-    float A,B,C;
-    printf("Input A,B,C: ");
-    scanf("%f,%f,%f",&A,&B,&C);
-    if (A>B && A>C)
-        printf("Max is A: %.2f",A);
-    else if (B>A && B>C)
-        printf("Max is B: %.2f",B);
-    else
-        printf("Max is C: %.2f",C);
-    return 0;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     float A,B,C;
+//     printf("Input A,B,C: ");
+//     scanf("%f,%f,%f",&A,&B,&C);
+//     if (A>B && A>C)
+//         printf("Max is A: %.2f",A);
+//     else if (B>A && B>C)
+//         printf("Max is B: %.2f",B);
+//     else
+//         printf("Max is C: %.2f",C);
+//     return 0;
+// }
---
+++ VD tren lop buoi 4/Ex4 - 15-4-2024.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+//
+// int main()
+// {
+//     char letter;
+//     do
+//     {
+//         printf("\nInput any character: ");
+//         letter = getchar();
+//         if (letter >= 65 && letter <= 128)
+//         {
+//             printf("\nYour character in Alphabet");
+//
+//             if (letter == 65 || letter == 69 || letter == 73 || letter == 79 || letter == 85 ||
+//                 letter == 97 || letter == 101 || letter == 105 || letter == 111 || letter == 117)
+//                 printf("\nYour character is \"Nguyen Am\"");
+//             else
+//                 printf("\nYour character is \"Phu Am\"");
+//         }
+//         else
+//             printf("\nYour character not inside Alphabet, Again plz");
+//         fflush(stdin);
+//     }
+//     while (letter < 65 || letter > 128);
+//
+//     return 0;
+// }
