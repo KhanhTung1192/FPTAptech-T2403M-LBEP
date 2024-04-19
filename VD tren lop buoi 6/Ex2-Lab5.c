c690a68e69b463398ea506a13670683571c21866
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-19 08:50:15 +0700

    19-04-2024

--- VD tren lop buoi 6/Ex2-Lab5.c
+++ VD tren lop buoi 6/Ex2-Lab5.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-int main()
-{
-    int num,total=0;
-    printf("Input any Number");
-    scanf("%d",&num);
-    for(;num!=0;)
-    {
-        total+=num%10;
-        num=num/10;
-    }
-    printf("Total of number:%d",total);
-    return 0;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     int num,total=0;
+//     printf("Input any Number");
+//     scanf("%d",&num);
+//     for(;num!=0;)
+//     {
+//         total+=num%10;
+//         num=num/10;
+//     }
+//     printf("Total of number:%d",total);
+//     return 0;
+// }
--- VD tren lop buoi 6/Ex3-Lab5.c
+++ VD tren lop buoi 6/Ex3-Lab5.c
@@ -1,3 +1,17 @@
-//
-// Created by Nakai on 19/04/2024.
-//
+#include <stdio.h>
+int main()
+{
+    int row,i,f;
+    printf("Input how many row");
+    scanf("%d",&row);
+    for(f=0;f<=row;f++)
+    {
+        for(i=0;i<f;i++)
+            {
+            printf("*");
+            }
+        printf("\n");
+
+    }
+    return 0;
+}
