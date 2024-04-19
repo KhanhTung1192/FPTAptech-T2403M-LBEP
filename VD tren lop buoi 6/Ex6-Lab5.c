2820d1f97103e1d77dea2d1aad39fa46aac89a37
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-19 09:51:59 +0700

    19-04-2024

--- VD tren lop buoi 6/Ex6-Lab5.c
+++ VD tren lop buoi 6/Ex6-Lab5.c
@@ -1,26 +1,54 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int num,f;
+//     char true;
+//     Chapter1:
+//     do
+//     {
+//         printf("Input number:");
+//         scanf("%d",&num);
+//         if (num<1||num>9)
+//             printf("Again plz\n");
+//     } while (num<1||num>9);
+//
+//
+//         for (f=1;f<=9;f++)
+//         {
+//             printf("%d*%d=%d\n",num,f,num*f);
+//         }
+//         printf("do you want Continue (Y/N): ");
+//         scanf(" %s",&true);
+//         if (true=='y' || true=='Y')
+//             goto Chapter1;
+//     fflush(stdin);
+//
+//     return 0;
+// }
 #include <stdio.h>
 int main()
 {
-    int num,i,f;
+    int num,f;
     char true;
+    Chapter1:
     do
     {
-        printf("Input how many:");
+        printf("Input number:");
         scanf("%d",&num);
         if (num<1||num>9)
             printf("Again plz\n");
     } while (num<1||num>9);
 
-    for (i=1;i!=num;i++)
+
+    for (f=1;f<=9;f++)
     {
-        for (f=1;f<=9;f++)
-        {
-            printf("%d*%d=%d\n",i,f,i*f);
-        }
-        printf("do you want Continue (Y/N): ");
-        scanf(" %s",&true);
-        if (true!='y' && true!='Y')
-            break;
+        printf("%d*%d=%d\n",num,f,num*f);
     }
+    printf("do you want Continue (Y/N): ");
+    scanf(" %s",&true);
+    if (true=='y' || true=='Y')
+        goto Chapter1;
+    fflush(stdin);
+
     return 0;
 }
