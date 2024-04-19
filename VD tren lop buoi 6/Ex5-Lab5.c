ecd712fc11332e63a64d7700d454dc846b8ec957
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-19 09:35:50 +0700

    19-04-2024

--- VD tren lop buoi 6/Ex5-Lab5.c
+++ VD tren lop buoi 6/Ex5-Lab5.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-int main()
-{
-    int num,result=0;
-    do
-        {
-        printf("Input num: ");
-        scanf("%d",&num);
-        if(num>=0 && num<100)
-            result++;
-    } while(num>=0 && num<100);
-    if (num<0 || num>=100)
-    {
-        printf("Input wrong");
-    }
-    printf("Have %d num inside 0-100",result);
-}
+// #include <stdio.h>
+// int main()
+// {
+//     int num,result=0;
+//     do
+//         {
+//         printf("Input num: ");
+//         scanf("%d",&num);
+//         if(num>=0 && num<100)
+//             result++;
+//     } while(num>=0 && num<100);
+//     if (num<0 || num>=100)
+//     {
+//         printf("Input wrong");
+//     }
+//     printf("Have %d num inside 0-100",result);
+// }
--- VD tren lop buoi 6/Ex6-Lab5.c
+++ VD tren lop buoi 6/Ex6-Lab5.c
@@ -1,3 +1,26 @@
-//
-// Created by Nakai on 19/04/2024.
-//
+#include <stdio.h>
+int main()
+{
+    int num,i,f;
+    char true;
+    do
+    {
+        printf("Input how many:");
+        scanf("%d",&num);
+        if (num<1||num>9)
+            printf("Again plz\n");
+    } while (num<1||num>9);
+
+    for (i=1;i!=num;i++)
+    {
+        for (f=1;f<=9;f++)
+        {
+            printf("%d*%d=%d\n",i,f,i*f);
+        }
+        printf("do you want Continue (Y/N): ");
+        scanf(" %s",&true);
+        if (true!='y' && true!='Y')
+            break;
+    }
+    return 0;
+}
