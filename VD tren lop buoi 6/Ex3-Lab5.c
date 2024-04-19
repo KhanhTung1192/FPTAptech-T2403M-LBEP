1bae7a2a9f616de6ddeae8c1586dc511caa5ceeb
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-19 09:06:48 +0700

    19-04-2024

--- VD tren lop buoi 6/Ex3-Lab5.c
+++ VD tren lop buoi 6/Ex3-Lab5.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-int main()
-{
-    int row,i,f;
-    printf("Input how many row");
-    scanf("%d",&row);
-    for(f=0;f<=row;f++)
-    {
-        for(i=0;i<f;i++)
-            {
-            printf("*");
-            }
-        printf("\n");
-
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     int row,i,f;
+//     printf("Input how many row");
+//     scanf("%d",&row);
+//     for(f=0;f<=row;f++)
+//     {
+//         for(i=0;i<f;i++)
+//             {
+//             printf("*");
+//             }
+//         printf("\n");
+//
+//     }
+//     return 0;
+// }
---
+++ VD tren lop buoi 6/Ex4-Lab5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char letter;
+    do
+    {
+        printf("\nInput any character: ");
+        letter=getchar();
+        if (letter<32 || letter>126)
+            printf("Input again");
+    } while(letter<32 || letter>126);
+
+        if(letter==32)
+            printf("\nexit");
+        else if (letter>=48 && letter<=57)
+            printf("\nYour character is Number");
+        else if(letter>=65 && letter<=90 || letter>=97 && letter<=122)
+            printf("\nYour character is character");
+        else
+            printf("\nYour character is Special Symbol");
+        // fflush(stdin);
+
+
+    return 0;
+}
