284c4b4cb75d1ba64ddaa32c8906820538b9874d
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-19 09:22:27 +0700

    19-04-2024

--- VD tren lop buoi 6/Ex5-Lab5.c
+++ VD tren lop buoi 6/Ex5-Lab5.c
@@ -1,3 +1,17 @@
-//
-// Created by Nakai on 19/04/2024.
-//
+#include <stdio.h>
+int main()
+{
+    int num,result=0;
+    do
+        {
+        printf("Input num: ");
+        scanf("%d",&num);
+        if(num>=0 && num<100)
+            result++;
+    } while(num>=0 && num<100);
+    if (num<0 || num>=100)
+    {
+        printf("Input wrong");
+    }
+    printf("Have %d num inside 0-100",result);
+}
