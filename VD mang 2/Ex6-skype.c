9233bf4ef2f80d70240448eed1d53110ac133296
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-22 12:03:43 +0700

    22-04-2024

---
+++ VD mang 2/Ex6-skype.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define a 5
+int main()
+{
+    char arr[a][30];
+    for (int i=0;i<a;i++)
+    {
+        printf("input name %d: ",i+1);
+        scanf("%[^\n]s",&arr[i]);
+        fflush(stdin);
+    }
+    for (int i=0;i<a;i++)
+    {
+        printf("%s\t",arr[i]);
+    }
+    return 0;
+}
