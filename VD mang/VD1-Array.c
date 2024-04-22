d83ef1cffb046ee1db9ec2a6fb4f43398a3e6b90
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-22 09:13:11 +0700

    19-04-2024

--- VD mang/VD1-Array.c
+++ VD mang/VD1-Array.c
@@ -1,3 +1,11 @@
-//
-// Created by Nakai on 22/04/2024.
-//
+// #include <stdio.h>
+// int main()
+// {
+//     char arr1[26], arr2[26];
+//     for(int i=0;i<26;i++)
+//     {
+//         arr1[i]=i+65;arr2[i]=i+97;
+//         printf("%c-%c\t",arr1[i],arr2[i]);
+//     }
+//     return 0;
+// }
