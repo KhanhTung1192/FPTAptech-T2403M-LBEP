c38378d93cfdba4f2ea1b6cfaf4653eea7885f36
KhanhTung1192 <tunglk1192@gmail.com>
2024-04-26 08:22:13 +0700

    26-04-2024

--- Buoi 8 26-4/VD1-class.c
+++ Buoi 8 26-4/VD1-class.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-int main()
-{
-    //input
-    int num;
-    printf("Input num: ");
-    scanf("%d",&num);
-    //output
-    printf("\n");
-    for (int i=0;i<num;i++)
-    {
-        for(int j=0;j<num;j++)
-        {
-            printf("* ");
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for (int i=1;i<=num;i++)
-    {
-        for(int j=0;j<i;j++)
-        {
-            printf("* ");
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     //input
+//     int num;
+//     printf("Input num: ");
+//     scanf("%d",&num);
+//     //output
+//     printf("\n");
+//     for (int i=0;i<num;i++)
+//     {
+//         for(int j=0;j<num;j++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     printf("\n");
+//     for (int i=1;i<=num;i++)
+//     {
+//         for(int j=0;j<i;j++)
+//         {
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
