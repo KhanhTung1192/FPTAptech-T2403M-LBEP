379be6d77d9d96343fcc81c7fa95f1f000cb02af
KhanhTung1192 <tunglk1192@gmail.com>
2024-05-03 23:09:44 +0700

    26-04-2024

--- Buoi-10 - 5-3/VD4-skype.c
+++ Buoi-10 - 5-3/VD4-skype.c
@@ -1,38 +1,38 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// int random(int a);
-// int maximum(int a[],int b);
-// void main()
-// {
-//     int a;
-//     printf("nhap so luong mang so nguyen: ");
-//     scanf("%d",&a);
-//     int Function[a];
-//     srand(time(NULL));
-//     for(int i=0;i<a;i++)
-//     {
-//         Function[i]=random(a);
-//     }
-//     printf("\nGia tri lon nhat: %d",maximum(Function,a));
-// }
-//
-// int random(int a)
-// {
-//     {
-//         a=rand()%1000;
-//         printf("%d\t",a);
-//     }
-//     return a;
-// }
-//
-// int maximum(int a[],int b)
-// {
-//     int max=a[0];
-//     for(int j=1;j<b;j++)
-//     {
-//         if(a[j]>max)
-//             max=a[j];
-//     }
-//     return max;
-// }
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int random(int a);
+int maximum(int a[],int b);
+void main()
+{
+    int a;
+    printf("nhap so luong mang so nguyen: ");
+    scanf("%d",&a);
+    int Function[a];
+    srand(time(NULL));
+    for(int i=0;i<a;i++)
+    {
+        Function[i]=random(a);
+    }
+    printf("\nGia tri lon nhat: %d",maximum(Function,a));
+}
+
+int random(int a)
+{
+    {
+        a=rand()%1000;
+        printf("%d\t",a);
+    }
+    return a;
+}
+
+int maximum(int a[],int b)
+{
+    int max=a[0];
+    for(int j=1;j<b;j++)
+    {
+        if(a[j]>max)
+            max=a[j];
+    }
+    return max;
+}
